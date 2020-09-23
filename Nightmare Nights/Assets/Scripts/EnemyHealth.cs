4d1a5df3ae414bc37df1d6c23f3bcbee4c8860fb
mjLamarree <67963551+mjLamarree@users.noreply.github.com>
2020-09-23 17:16:57 -0400

    Started Combat System
    Started work on the combat system not much progress yet

---
+++ Nightmare Nights/Assets/Scripts/EnemyHealth.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+    public int maxHP;
+    public int currentHP;
+    public int regenRate;
+    private int damageCounter;
+    private int regenCount;
+
+    public void Update()
+    {
+        if(currentHP <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Enemy Base") && regenCount == 0)
+        {
+            StartCoroutine("HealOverTime", regenRate);
+            regenCount++;
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Weapon") && damageCounter == 0)
+        {
+            TakeDamage(collision.GetComponent<WeaponScript>().damageDealt);
+            damageCounter++;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Enemy Base"))
+        {
+            StopCoroutine("HealOverTime");
+            regenCount = 0;
+        }
+
+        if (collision.CompareTag("Weapon"))
+        {
+            damageCounter = 0;
+        }
+    }
+
+
+
+
+
+
+    public void TakeDamage(int damage)
+    {
+        currentHP -= damage;
+    }
+
+    public IEnumerator HealOverTime(int regenRate)
+    {
+        while (currentHP < maxHP)
+        {
+            yield return new WaitForSeconds(regenRate);
+            currentHP += 1;
+        }
+        
+    }
+    
+    
+}
---
+++ Nightmare Nights/Assets/Scripts/WeaponScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponScript : MonoBehaviour
+{
+    public int damageDealt;
+    public int weaponSpeed;
+
+    void Start()
+    {
+        
+    }
+
+    
+    void Update()
+    {
+
+    }
+
+    public void AttackWithStab()
+    {
+
+    }
+
+    public void AttackWithSwing()
+    {
+
+    }
+
+    
+}
