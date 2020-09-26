0a53343073cc825a35f8a2ae1b2d3fa0c62e8399
ProjectCBL <projectxcbl@gmail.com>
2020-09-26 14:55:36 -0400

    Added a class to define characters

---
+++ Nightmare Nights/Assets/Scripts/Entity.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Entity : MonoBehaviour
+{
+
+    public enum CharacterStates {
+        ALIVE,
+        DEAD
+    }
+
+    protected string characterName;
+    protected int maxHealth;
+    protected int currentHealth;
+
+    public Entity(string characterName, int maxHealth, int currentHealth)
+    {
+        this.characterName = characterName;
+        this.maxHealth = maxHealth;
+        this.currentHealth = currentHealth;
+    }
+
+    public void TakeDamage(int recievedDamage)
+    {
+        currentHealth -= recievedDamage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+    }
+
+}
