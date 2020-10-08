0954ed359d3c5593f42ca2509d307bcde3f66bab
ProjectCBL <projectxcbl@gmail.com>
2020-10-07 21:29:18 -0400

    Dungeon layout update

---
+++ Nightmare Nights/Assets/Scripts/DoorController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorController : MonoBehaviour
+{
+
+    public bool isDoorBlockedByEncounter = false;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        
+        if (collision.collider.tag == "Player" 
+            && isDoorBlockedByEncounter == false
+            && SecondAreaEventSystem.current.obtainedKeys == 3
+            )
+        {
+            gameObject.SetActive(false);
+        }
+
+    }
+
+}
