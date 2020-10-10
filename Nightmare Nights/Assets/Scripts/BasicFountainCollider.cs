cfeb9043ba58911beb5cb73f54d805f2f43efc04
ProjectCBL <projectxcbl@gmail.com>
2020-10-10 03:54:22 -0400

    Created basic universal fountain scripts

---
+++ Nightmare Nights/Assets/Scripts/BasicFountainCollider.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicFountainCollider : MonoBehaviour
+{
+
+    [SerializeField] private int healingRecieved = 5;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+
+        if (collision.collider.tag == "Player"
+            && collision.collider.GetComponent<DungeonPlayerCharacter>().currentHp != collision.collider.GetComponent<DungeonPlayerCharacter>().maxHp)
+        {
+            collision.collider.GetComponent<DungeonPlayerCharacter>().HealHealth(healingRecieved);
+            transform.parent.parent.GetComponent<BasicFountainAssetSwitcher>().SwitchFountainAsset();
+        }
+
+    }
+
+}
